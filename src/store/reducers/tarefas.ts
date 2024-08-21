d15feaedd6f8b331f13c65c1d2bbe6feb544cc03
Rachel Peppe <rapeppe@hotmail.com>
2024-08-21 15:05:45 -0300

    Projeto tarefas

---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,70 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+
+type TarefaState = {
+  itens: Tarefa[]
+}
+
+const initialState: TarefaState = {
+  itens: [
+    {
+      id: 1,
+      descricao: 'Estudar JavaScript revendo o exercício do módulo 2',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.CONCLUIDA,
+      titulo: 'Estudar JavaScript'
+    },
+    {
+      id: 2,
+      descricao: 'Estudar material de apoio',
+      prioridade: enums.Prioridade.NORMAL,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar TypeScript'
+    },
+    {
+      id: 3,
+      descricao: 'Praticar a construção de uma landong page',
+      prioridade: enums.Prioridade.IMPORTANTE,
+      status: enums.Status.PENDENTE,
+      titulo: 'Estudar Bootstrap'
+    }
+  ]
+}
+
+const tarefaSlice = createSlice({
+  name: 'tarefas',
+  initialState,
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state.itens = [
+        ...state.itens.filter((tarefa) => tarefa.id !== action.payload)
+      ]
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
+    },
+    cadastrar: (state, action: PayloadAction<Tarefa>) => {
+      const tarefaJaExiste = state.itens.find(
+        (tarefa) =>
+          tarefa.titulo.toLowerCase() === action.payload.titulo.toLowerCase()
+      )
+
+      if (tarefaJaExiste) {
+        alert('Já existe uma tarefa com esse nome')
+      } else {
+        state.itens.push(action.payload)
+      }
+    }
+  }
+})
+
+export const { remover, editar, cadastrar } = tarefaSlice.actions
+
+export default tarefaSlice.reducer
